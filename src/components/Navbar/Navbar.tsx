e63cc3fc217d0dd4e7d20c24ee8b8631b1e5bebc
Harun Sabljaković <harun_sabljakovic@hotmail.com>
2022-02-02 23:15:07 +0100

    add navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,33 @@
 import React from "react";
+import Pill from "../Pill";
+import Action from "./Action";
 import "./navbar.css";
+import { ReactComponent as Heart } from "../../assets/heart.svg";
+import { ReactComponent as PieChart } from "../../assets/pie-chart.svg";
 
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div className={"container"}>
+      <div className={"pill-container"}>
+        <Pill
+          backgroundColor="#FF98A8"
+          label="DZ Hospital"
+          styles={{ marginLeft: 20 }}
+        ></Pill>
+      </div>
+      <div className={"actions"}>
+        <Action
+          Icon={Heart}
+          styles={{ stroke: "#FF98A8", marginRight: 5 }}
+          title={"Drives"}
+        />
+        <Action
+          Icon={PieChart}
+          styles={{ stroke: "#FF98A8", marginRight: 5 }}
+          title={"Reports"}
+        />
+      </div>
+      <div></div>
+    </div>
+  );
 }
--- src/stories/Navbar/Navbar.stories.tsx
+++ src/stories/Navbar/Navbar.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Navbar from "../../components/Navbar";
 
 export default {
-  title: "Example/Dropdown",
+  title: "Example/Navbar",
   component: Navbar,
 } as ComponentMeta<typeof Navbar>;
 
