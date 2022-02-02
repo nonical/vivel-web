750de9b0a79856379b7872c18112372c57811958
Harun Sabljaković <harun_sabljakovic@hotmail.com>
2022-02-02 22:24:07 +0100

    initial

---
+++ src/stories/Navbar/Navbar.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Navbar from "../../components/Navbar";
+
+export default {
+  title: "Example/Dropdown",
+  component: Navbar,
+} as ComponentMeta<typeof Navbar>;
+
+const Template: ComponentStory<typeof Navbar> = (args) => <Navbar />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {};
