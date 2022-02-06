3decc9bd59eb428a16b97a89d9d329cc5616540c
Harun Sabljaković <harun_sabljakovic@hotmail.com>
2022-02-06 23:53:38 +0100

    initial

--- src/components/Modals/FinishDonation/FinishDonation.tsx
+++ src/components/Modals/FinishDonation/FinishDonation.tsx
@@ -10,40 +10,66 @@ export default function FinishDonation() {
     { value: "Failed", label: "Failed" },
     { value: "Canceled", label: "Canceled" },
   ];
+
+  const [donationStatus, setdonationStatus] = React.useState<
+    string | undefined
+  >("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const data = new FormData(e.target.form);
+    for (var pair of data.entries()) {
+      console.log(pair[0] + ", " + pair[1]);
+    }
+  };
+
   return (
-    <div className={"modal-container"}>
-      <div className={"modal-title"}>Finish Donation</div>
-      <div className={"finish-input-container"}>
-        <Dropdown
-          options={options}
-          label={"Status"}
-          placeHolder={"Select donation status"}
-        />
+    <form onSubmit={handleSubmit}>
+      <div className={"modal-container"}>
+        <div className={"modal-title"}>Finish Donation</div>
+        <div className={"finish-input-container"}>
+          <input
+            type="text"
+            hidden={true}
+            id={"status"}
+            name={"status"}
+            value={donationStatus}
+          />
+          <Dropdown
+            options={options}
+            label={"Status"}
+            selected={options[0]}
+            onChange={(value) => {
+              setdonationStatus(value?.value);
+            }}
+          />
+        </div>
+
+        <div className={"finish-input-container"}>
+          <Input
+            label={"Leukocyte Count (ccm)"}
+            name={"leukocyteCount"}
+            placeholder={"6,000"}
+          />
+        </div>
+        <div className={"finish-input-container"}>
+          <Input
+            label={"Erythrocyte Count (ccm)"}
+            name={"erythrocyteCount"}
+            placeholder={"4,520,000"}
+          />
+        </div>
+        <div className={"finish-input-container"}>
+          <Input
+            label={"Platelet Count (ml)"}
+            name={"plateletCount"}
+            placeholder={"329,000"}
+          />
+        </div>
+        <div className={"finish-input-container"}>
+          <Button label={"Finish Donation"} onClick={handleSubmit} />
+        </div>
       </div>
-      <div className={"finish-input-container"}>
-        <Input
-          label={"Leukocyte Count (ccm)"}
-          name={"leukocytes"}
-          placeholder={"6,000"}
-        />
-      </div>
-      <div className={"finish-input-container"}>
-        <Input
-          label={"Erythrocyte Count (ccm)"}
-          name={"erythrocytes"}
-          placeholder={"4,520,000"}
-        />
-      </div>
-      <div className={"finish-input-container"}>
-        <Input
-          label={"Platelet Count (ml)"}
-          name={"platelets"}
-          placeholder={"329,000"}
-        />
-      </div>
-      <div className={"finish-input-container"}>
-        <Button label={"Finish Donation"} />
-      </div>
-    </div>
+    </form>
   );
 }
