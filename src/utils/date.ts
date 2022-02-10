2825ff43e2e3005121c81090153dae0343a6d239
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-10 22:33:13 +0100

    Add Luxon

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,18 @@
+import { DateTime } from "luxon";
+
+// TODO: Remove all usages of JS's Date and this function
 export function toIsoDate(date: Date) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
   return `${year}-${month}-${day}`;
+}
+
+export function toDateFormat(dateTime: DateTime): string {
+  return dateTime.toLocaleString(DateTime.DATE_SHORT);
+}
+
+export function toTimeFormat(dateTime: DateTime): string {
+  return dateTime.toLocaleString(DateTime.TIME_SIMPLE);
 }
