5cb0c41070efcb8d357ef96e24c308b976b22670
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-12 17:44:10 +0100

    Drive Overview View (#8)
    * Add DriveOverview actions
    
    * Add SVG icons
    
    * Add read-only Drive Overview page
    
    * Fix class names for DonationOverview component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import Drive from "./views/Drive"
+import Drive from "./views/Drive";
+import DonationOverview from "./views/DonationOverview";
+
 const queryClient = new QueryClient();
 
 function App() {
@@ -9,7 +11,8 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/drives" element={<Drive/>} />
+          <Route path="/drives" element={<Drive />} />
+          <Route path="/drives/:driveId" element={<DonationOverview />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
