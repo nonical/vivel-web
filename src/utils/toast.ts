85bdb4950157f722990a4b7f54efd5adfd33a0ce
Nedžad Alibegović <nedzad@nedzad.dev>
2022-02-14 21:58:33 +0100

    Refactor toast utils

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,12 +1,21 @@
 import { toast } from "react-toastify";
 
-export function displayErrors(error: any): void {
-  const errors = JSON.parse(error.message).errors;
-  Object.values(errors).forEach((x: any) => {
-    x.forEach((y: string) => toast(y));
-  });
+export function displayErrors(
+  error: any,
+  unknownErrorMessage = "Unknown error occured!"
+): void {
+  try {
+    const errors = JSON.parse(error.message).errors;
+
+    Object.values(errors).forEach((x: any) => {
+      x.forEach((y: string) => toast.error(y));
+    });
+  } catch {
+    console.error(error);
+    toast.error(unknownErrorMessage);
+  }
 }
 
-export function displaySuccess(): void {
-  toast.success("Successfully saved");
+export function displaySuccess(message: string = "Successfully saved!"): void {
+  toast.success(message);
 }
