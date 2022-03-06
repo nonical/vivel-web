dc1cb591f227573caa4fea2721f9673fa7eb0531
Harun Sabljakovic <harun.sabljakovic@edu.fit.ba>
2022-03-06 23:31:50 +0100

    use hospital from claim

--- src/views/Drive/actions.ts
+++ src/views/Drive/actions.ts
@@ -1,4 +1,5 @@
 import { DateTime } from "luxon";
+import { getDecodedAccessToken } from "../../utils/auth";
 import { ENDPOINTS } from "../../utils/config";
 import fetch from "../../utils/fetch";
 
@@ -12,7 +13,8 @@ export interface Drive {
 }
 
 export async function fetchDrives(status: string): Promise<Drive[]> {
-  const res = await fetch(ENDPOINTS.Drives + `?Status=${status}`);
+  const hospitalId = getDecodedAccessToken().hospital;
+  const res = await fetch(`${ENDPOINTS.Hospitals}/${hospitalId}/drives?Status=${status}`);
   const json = await res.json();
 
   const drives: Drive[] = json.results.map((drive: any) => ({
